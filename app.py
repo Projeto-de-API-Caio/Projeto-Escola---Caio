851f588d2774cff80502072e1fb6459678b00728
MatheusMarrer10 <matheus.marrer@aluno.faculdadeimpacta.com.br>
2025-03-19 19:40:36 -0300

    PUT e testes PUT 19:39

--- app.py
+++ app.py
@@ -9,100 +9,7 @@ escola = {
 }
 
 
-#GET do Capolupo---------------------------------------------------------------------------------------------------------
-
-
-@app.route("/alunos", methods=["GET"])
-def getAlunos():
-    return jsonify(escola["alunos"])
-
-@app.route("/professores", methods=["GET"])
-def getProfessores():
-    return jsonify(escola["professores"])
-
-@app.route("/turmas", methods=["GET"])
-def getTurmas():
-    return jsonify(escola["turmas"])
-
-
-#GET ID da Joicy S2-----------------------------------------------------------------------------------------------------
-
-
-@app.route("/alunos/<int:id>", methods=["GET"])
-def obter_aluno_por_id(id):
-    for aluno in escola["alunos"]:
-        if aluno.get("id") == id:
-            return jsonify(aluno)
-    return jsonify({"erro": "aluno nao encontrado"}), 400
-
-@app.route("/professores/<int:id>", methods=["GET"])
-def obter_professor_por_id(id):
-    for professor in escola["professores"]:
-        if professor.get("id") == id:
-            return jsonify(professor)
-    return jsonify({"erro": "professor nao encontrado"}), 400
-
-@app.route("/turma/<int:id>", methods=["GET"])
-def obter_turma_por_id(id):
-    for turma in escola["turmas"]:
-        if turma["id"] == id:
-            return jsonify(turma)
-    return jsonify({"erro": "turma nao encontrada"}), 400
-
-
-#Meu codigo (POST)FILIPE---------------------------------------------------------------------------------------------
-
-##confirmação
-@app.route("/alunos", methods=["POST"])
-def criarAluno():
-    dados = request.json
-    if "id" not in dados or "nome" not in dados:
-        return jsonify({"erro": "aluno sem nome"}), 400
-    
-    for aluno in escola["alunos"]:
-        if aluno["id"] == dados["id"]:
-            return jsonify({"erro": "id ja utilizada"}), 400
-    
-    novo_aluno = {
-        "id": dados["id"],
-        "nome": dados["nome"]
-    }
-    escola["alunos"].append(novo_aluno)
-    return jsonify(novo_aluno), 200
-
-@app.route("/professores", methods=["POST"])
-def criarProfessor():
-    dados = request.json
-    if "id" not in dados or "nome" not in dados:
-        return jsonify({"erro": "professor sem nome"}), 400
-    
-    for professor in escola["professores"]:
-        if professor["id"] == dados["id"]:
-            return jsonify({"erro": "id ja utilizada"}), 400
-    
-    novo_professor = {
-        "id": dados["id"],
-        "nome": dados["nome"]
-    }
-    escola["professores"].append(novo_professor)
-    return jsonify(novo_professor), 200
-
-@app.route("/turmas", methods=["POST"])
-def criarTurma():
-    dados = request.json
-    if "id" not in dados or "nome" not in dados or "alunos" not in dados:
-        return jsonify({"erro": "informacoes incompletas para criar turma"}), 400
-    
-    nova_turma = {
-        "id": dados["id"],
-        "nome": dados["nome"],
-        "alunos": dados["alunos"]
-    }
-    escola["turmas"].append(nova_turma)
-    return jsonify(nova_turma), 200
-
-
-#PUT do SEM MUNDIAL2(MATHEUS)--------------------------------------------------------------------------------------------
+#PUT (MATHEUS)--------------------------------------------------------------------------------------------
 
 
 @app.route("/alunos/<int:idAluno>", methods=["PUT"])
@@ -139,36 +46,6 @@ def updateTurma(idTurma):
             return jsonify(turma), 200
     return jsonify({"erro": "turma nao encontrada"}), 400
 
-
-#DELETAR DA VICTORIA------------------------------------------------------------------------------------------------------
-
-
-@app.route("/alunos/<int:idAluno>", methods=["DELETE"])
-def deleteAluno(idAluno):
-    aluno_existe = any(aluno['id'] == idAluno for aluno in escola["alunos"])
-    if not aluno_existe:
-        return jsonify({"erro": "aluno nao encontrado"}), 400
-
-    escola["alunos"] = [aluno for aluno in escola["alunos"] if aluno["id"] != idAluno]
-    return '', 204
-
-@app.route("/professores/<int:idProfessor>", methods=["DELETE"])
-def deleteProfessor(idProfessor):
-    professor_existe = any(prof['id'] == idProfessor for prof in escola["professores"])
-    if not professor_existe:
-        return jsonify({"erro": "professor nao encontrado"}), 400
-
-    escola["professores"] = [prof for prof in escola["professores"] if prof["id"] != idProfessor]
-    return '', 204
-
-@app.route("/turmas/<int:idTurma>", methods=["DELETE"])
-def deleteTurma(idTurma):
-    turma_existe = any(turma["id"] == idTurma for turma in escola["turmas"])
-    if not turma_existe:
-        return jsonify({"erro": "turma nao encontrada"}), 400
-
-    escola["turmas"] = [turma for turma in escola["turmas"] if turma["id"] != idTurma]
-    return '', 204
 
 
 # RESETAR DADOS
